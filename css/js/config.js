3f359c15b309905c6c97d0cc3d32ae75dce59678
swati nigam <swati.nigam567@gmail.com>
2023-07-20 17:21:28 +0530

    flappybird

---
+++ css/js/config.js
@@ -0,0 +1,71 @@
+/**
+ * The script just contains handlers for making the game configurable. Nothing of much interest here ^__^
+ * 
+ * Author @nellex
+ */
+
+var isEnvironmentStatic = true;
+var displayTarget = false;
+
+function gameSpeedChange(curSpeed) {
+    clearInterval(eventLoop);
+    eventLoop = setInterval(loop, 100-curSpeed);
+}
+
+function toggleDisplayTarget(showTarget) {
+    if (showTarget == "Yes") {
+        displayTarget = true;
+    } else {
+        displayTarget = false;
+    }
+}
+
+function environmentChange(curEnv) {
+    if (curEnv == "Static") {
+        isEnvironmentStatic = true;
+    } else {
+        isEnvironmentStatic = false;
+    }
+}
+
+function saveModel() {
+    window.localStorage.setItem("flappybird-qtable", JSON.stringify(Q_table));
+    alert("Model was saved successfully!");
+}
+
+function loadModel() {
+    if (window.localStorage.getItem("flappybird-qtable") != null) {
+        Q_table = JSON.parse(window.localStorage.getItem("flappybird-qtable"));
+        alert("Model was loaded successfully!");
+    } else {
+        alert("No saved model found in local storage");
+    }
+}
+
+var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
+
+function loadPreModel() {
+    var href = window.location.href;
+    var host = href.substring(0, href.lastIndexOf('/'));
+    getJSON(host + "/model/qtable-x3-y6.json").then(function(data) {
+        Q_table = eval(data);
+        alert("Model loaded successfully!");
+    }, function(status) {
+    alert("Failure in loading pre-trained model");
+    });
+}
